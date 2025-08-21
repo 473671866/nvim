10aa013939d9b15d8a0aac71944da1900386f02a
cumulonimbus <cumulonimbus@qq.com>
2025-08-21 09:45:49 +0800

    init

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night" },
+  },
+
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("onedark").setup({
+        style = "darker",
+      })
+      -- Enable theme
+      require("onedark").load()
+    end,
+  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "mitch1000/backpack",
+  --   },
+  -- },
+}
